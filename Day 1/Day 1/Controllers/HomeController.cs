e5a43b6542572a4e65614abcbd76d94d5bc6ab51
Abdallah-Alrayyan <aboodrayan300@gmail.com>
2025-02-16 23:54:56 +0300

    Complete Day 1

---
+++ Day 1/Day 1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Day_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Offers()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
